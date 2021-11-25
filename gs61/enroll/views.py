b6f9325527d325e4bd90b4bdc46b040ebe0b9d2f
Rich Hildred <rahamat@veeaargroup.com>
2021-11-25 19:18:43 +0530

    loginproject

--- gs61/enroll/views.py
+++ gs61/enroll/views.py
@@ -1,15 +1,16 @@
 from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
-
+from .forms import SignUpForm
+from django.contrib import messages
 
 # Create your views here.
 def sign_up(request):
     if request.method=="POST":
-        fm=UserCreationForm(request.POST)
+        fm=SignUpForm(request.POST)
         if fm.is_valid():
+            messages.success(request,'Account Create Successfully !!')
             fm.save()
     else:
-        fm=UserCreationForm()
+        fm=SignUpForm()
     return render(request,'enroll/signup.html',{'form':fm})
 
 
